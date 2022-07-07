bd0417d881b46aa377efecd1a9b22b7b06037806
Kishor <kishor1124@gmail.com>
2022-07-07 15:42:43 +0530

    first commit

---
+++ src/main/java/com/aris/global/ServiceRegisteryAppApplication.java
@@ -0,0 +1,15 @@
+package com.aris.global;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegisteryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisteryAppApplication.class, args);
+	}
+
+}
